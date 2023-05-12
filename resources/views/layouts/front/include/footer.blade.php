ef5dc80a90ade915018bbd107b09f238bc968700
avkashgoyal <edusemarketing0310@gmail.com>
2023-05-13 02:21:47 +0530

    footer

--- resources/views/layouts/front/include/footer.blade.php
+++ resources/views/layouts/front/include/footer.blade.php
@@ -1,10 +1,9 @@
 <footer class="footer">
 <div class="container bottom_border">
 <div class="row">
-<div class=" col-sm-4 col-md col-sm-4  col-12 col">
+    <div class=" col-sm-4 col-md-4  col-lg-4 col-12">
 <h5 class="headin5_amrc col_white_amrc pt2">Find us</h5>
 <!--headin5_amrc-->
-<p class="mb10">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
 <p><i class="fa fa-location-arrow"></i>  1/59 Darbar Nagar, Magob, Puna Patia Surat</p>
 <p><i class="fa fa-phone"></i>  +91-8273028229</p>
 <p><i class="fa fa fa-envelope"></i> info@maiyee.in  </p>
@@ -13,7 +12,7 @@
 </div>
 
 
-<div class=" col-sm-4 col-md  col-6 col">
+<div class=" col-sm-4 col-md-4  col-lg-4 col-12">
 <h5 class="headin5_amrc col_white_amrc pt2">Quick links</h5>
 <!--headin5_amrc-->
 <ul class="footer_ul_amrc">
@@ -28,7 +27,7 @@
 </div>
 
 
-<div class=" col-sm-4 col-md  col-6 col">
+<div class=" col-sm-4 col-md-4  col-lg-4 col-12">
 <h5 class="headin5_amrc col_white_amrc pt2">Important Links</h5>
 <!--headin5_amrc-->
 <ul class="footer_ul_amrc">
@@ -43,17 +42,16 @@
 </div>
 
 
-<div class=" col-sm-4 col-md  col-12 col">
+<!--<div class=" col-sm-4 col-md  col-12 col">
 <h5 class="headin5_amrc col_white_amrc pt2">Follow us</h5>
-<!--headin5_amrc ends here-->
+
 
 <ul class="footer_ul2_amrc">
 <li><a href="#"><i class="fab fa-instagram fleft padding-right"></i> </a><p>Lorem Ipsum is simply dummy text of the printing...<a href="#">https://www.lipsum.com/</a></p></li>
 <li><a href="#"><i class="fab fa-instagram fleft padding-right"></i> </a><p>Lorem Ipsum is simply dummy text of the printing...<a href="#">https://www.lipsum.com/</a></p></li>
 <li><a href="#"><i class="fab fa-instagram fleft padding-right"></i> </a><p>Lorem Ipsum is simply dummy text of the printing...<a href="#">https://www.lipsum.com/</a></p></li>
 </ul>
-<!--footer_ul2_amrc ends here-->
-</div>
+</div>--->
 </div>
 </div>
 
