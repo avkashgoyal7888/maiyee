2a6b4ff43d68944d16a4de91b905635dd814542c
avkashgoyal <edusemarketing0310@gmail.com>
2023-05-18 02:23:08 +0530

    chnages

--- resources/views/layouts/front/include/nav.blade.php
+++ resources/views/layouts/front/include/nav.blade.php
@@ -1,7 +1,7 @@
 <!--Promotion Bar-->
 <style>
     body  {
-      background-image: url("{{asset('front/assets/images/background.jpeg')}}");
+      background-image: linear-gradient(to right, rgba(166, 77, 255,0), rgba(166, 77, 255,0.5));
     }
     </style>
 <div class="notification-bar mobilehide">
