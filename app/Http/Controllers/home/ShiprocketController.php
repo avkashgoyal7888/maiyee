0f08213b0629ae4bba473d1e4d0af5d408f93c27
aneesh0987 <82146611+aneesh723@users.noreply.github.com>
2023-05-23 12:24:04 +0530

    'push'

--- app/Http/Controllers/home/ShiprocketController.php
+++ app/Http/Controllers/home/ShiprocketController.php
@@ -297,7 +297,7 @@ class ShiprocketController extends Controller
             'shipping_is_billing' => true,
             'order_items' => $order_items,
             'payment_method' => $payment,
-            'shipping_charges' => $shipping,
+            'shipping_charges' => 0.00,
             'giftwrap_charges' => 0.00,
             'transaction_charges' => 0.00,
             'total_discount' => 0.00,
