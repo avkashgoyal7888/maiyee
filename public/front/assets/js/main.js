86dd33e0c17e45d006ce90309ab7738ffbbe9466
avkashgoyal <edusemarketing0310@gmail.com>
2023-05-29 15:55:50 +0530

    changes

--- public/front/assets/js/main.js
+++ public/front/assets/js/main.js
@@ -253,14 +253,14 @@
 		{
 		  breakpoint: 1024,
 		  settings: {
-			slidesToShow: 4,
+			slidesToShow: 6,
 			slidesToScroll: 1
 		  }
 		},
 		{
 		  breakpoint: 600,
 		  settings: {
-			slidesToShow: 2,
+			slidesToShow: 3,
 			slidesToScroll: 1
 		  }
 		},
