674058d92fdbce6ab65cd849c58b59264ba8f0a3
aneesh0987 <82146611+aneesh723@users.noreply.github.com>
2023-05-31 14:51:00 +0530

    'push'

--- app/Http/Controllers/home/PaymentController.php
+++ app/Http/Controllers/home/PaymentController.php
@@ -94,8 +94,88 @@ class PaymentController extends Controller
             return view('front.cancel', compact('cartNav', 'cartTotalnav', 'cartCount', 'nav', 'cat', 'order'));
     }
 
-    public function orderSuccess()
+    public function orderSuccess(Request $request)
     {
+        $cancelData = $request->all();
+        $ref = $cancelData['txnid'];
+        $txnid = $cancelData['mihpayid'];
+        $status = $cancelData['status'];
+        $order = Order::where('order_id', $ref)->first();
+        if ($order) {
+            // Update the order status to indicate cancellation
+            $order->txnid = $txnid;
+            $order->order_status = $status;
+            $order->update();
+    
+        }
+
+        $client = new Client([
+            'base_uri' => 'https://apiv2.shiprocket.in/v1/',
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Authorization' => 'Basic '.base64_encode('<tech@maiyee.in>:<Eduse@123>')
+            ]
+        ]);
+
+        // Authenticate the user
+        $response = $client->post('external/auth/login', [
+            'json' => [
+                'email' => 'tech@maiyee.in',
+                'password' => 'Eduse@123',
+            ]
+        ]);
+
+        if ($response->getStatusCode() == 200) {
+            $result = json_decode($response->getBody()->getContents());
+            $access_token = $result->token;
+            $order_details = OrderDetail::where('order_id', $ref)->get();
+
+            $order_items = [];
+                foreach ($order_details as $order_detail) {
+                    $order_items[] = [
+                        'name' => $order_detail->product->name,
+                        'sku' => $order_detail->product->style_code,
+                        'units' => $order_detail->quantity,
+                        'selling_price' => $order_detail->price,
+                        'discount' => 0.00,
+                        'tax' => 0.00,
+                    ];
+                }
+
+                $orderData = [
+                    'order_id' => $order->order_id,
+                    'order_date' => $order->order_date,
+                    'billing_customer_name' => $order->name,
+                    'billing_last_name' => '',
+                    'billing_address' => $order->address,
+                    'billing_city' => $order->city,
+                    'billing_pincode' => $order->pin_code,
+                    'billing_state' => $order->state,
+                    'billing_country' => 'INDIA',
+                    'billing_email' => $order->email,
+                    'billing_phone' => $order->contact,
+                    'shipping_is_billing' => true,
+                    'order_items' => $order_items,
+                    'payment_method' => 'prepaid',
+                    'shipping_charges' => 0.00,
+                    'giftwrap_charges' => 0.00,
+                    'transaction_charges' => 0.00,
+                    'total_discount' => 0.00,
+                    'sub_total' => $order->payable,
+                    'length' => 12.00,
+                    'breadth' => 10.00,
+                    'height' => 1.00,
+                    'weight' => 0.300,
+                ];
+
+                $response = $client->post('external/orders/create/adhoc', [
+        'headers' => [
+            'Content-Type' => 'application/json',
+            'Authorization' => 'Bearer ' . $access_token,
+        ],
+        'json' => $orderData,
+    ]);
+        }
         $cartNav = Cart::get();
         $cartTotalnav = 0;
         $cartCount = 0;
@@ -110,8 +190,20 @@ class PaymentController extends Controller
         return view('front.success',compact('cartNav','cartTotalnav','cartCount','nav','cat','order'));
     }
 
-    public function orderFail()
+    public function orderFail(Request $request)
     {
+        $cancelData = $request->all();
+        $ref = $cancelData['txnid'];
+        $txnid = $cancelData['mihpayid'];
+        $status = $cancelData['status'];
+        $order = Order::where('order_id', $ref)->first();
+        if ($order) {
+            // Update the order status to indicate cancellation
+            $order->txnid = $txnid;
+            $order->order_status = $status;
+            $order->update();
+    
+        }
         $cartNav = Cart::get();
         $cartTotalnav = 0;
         $cartCount = 0;
