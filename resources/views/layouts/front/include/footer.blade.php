a3192142b169d1d194ed5452e1e529fdeb4c7db1
avkashgoyal <edusemarketing0310@gmail.com>
2023-06-10 15:10:35 +0530

    chnges

--- resources/views/layouts/front/include/footer.blade.php
+++ resources/views/layouts/front/include/footer.blade.php
@@ -2,12 +2,12 @@
 <div class="container bottom_border">
 <div class="row justify-content-center">
 
-<div class=" col-sm-6 col-md-4  col-lg-4 col-12">
+<div class=" col-sm-6 col-md-4  col-lg-4 col-12" style="text-align: center">
 <h5 class="headin5_amrc col_white_amrc pt2">Address</h5>
-<p><i class="fa fa-location-arrow"></i>  3006 Shree Kuberji Empire, Kadodara Road, Saroli, Surat Gujrat (395010)</p>
+<p><i class="fa fa-location-arrow"></i>  3006 Shree Kuberji Empire, Kadodara Road, <br>Saroli, Surat Gujrat (395010)</p>
 </div>
 
-<div class=" col-sm-6 col-md-4  col-lg-4 col-12">
+<div class=" col-sm-6 col-md-4  col-lg-4 col-12" style="text-align: center">
     <h5 class="headin5_amrc col_white_amrc pt2">Contact Us</h5>
     <p><i class="fa fa-phone"></i>  +91-9904145427</p>
     <p><i class="fa fa fa-envelope"></i> info@maiyee.in  </p>
@@ -20,7 +20,7 @@
     </div>
 
 
-<div class=" col-sm-6 col-md-4  col-lg-4 col-12">
+<div class=" col-sm-6 col-md-4  col-lg-4 col-12" style="text-align: center">
 <h5 class="headin5_amrc col_white_amrc pt2">Important Links</h5>
 <!--headin5_amrc-->
 <ul class="footer_ul_amrc">
@@ -40,12 +40,6 @@
 <!--foote_bottom_ul_amrc ends here-->
 <p class="text-center">Copyright @2023 At Maiyee | Designed By <a href="#">EduTech</a></p>
 
-<ul class="social_footer_ul">
-<li><a href="https://www.facebook.com/profile.php?id=100070799291176&mibextid=ZbWKwL" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
-<li><a href="https://pin.it/2kbR7xN" target="_blank"><i class="fab fa-pinterest"></i></a></li>
-<li><a href="https://youtube.com/@Maiyee_collection" target="_blank"><i class="fab fa-youtube"></i></a></li>
-<li><a href="https://instagram.com/maiyee.in?igshid=ZDdkNTZiNTM=" target="_blank"><i class="fab fa-instagram"></i></a></li>
-</ul>
 <!--social_footer_ul ends here-->
 </div>
 
