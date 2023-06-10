c851093b01cf05b45d78edd9a59a4306cce2ec56
avkashgoyal <edusemarketing0310@gmail.com>
2023-06-10 14:47:38 +0530

    chnges

--- resources/views/front/product-detail.blade.php
+++ resources/views/front/product-detail.blade.php
@@ -69,11 +69,6 @@
                      <div class="zoompro-span">
                         <img id="zoompro-image" class="blur-up lazyload zoompro" alt="" src="{{ asset('admin/color/'.$proimage->where('color_id', app('request')->input('color_id', $proimage[0]->color_id))->first()->image) }}" />
                      </div>
-                     <div class="product-labels"><span class="lbl on-sale">Sale</span><span class="lbl pr-label1">new</span></div>
-                     <div class="product-buttons">
-                        <a href="https://www.youtube.com/watch?v=93A2jOW5Mog" class="btn popup-video" title="View Video"><i class="icon anm anm-play-r" aria-hidden="true"></i></a>
-                        <a href="#" class="btn prlightbox" title="Zoom"><i class="icon anm anm-expand-l-arrows" aria-hidden="true"></i></a>
-                     </div>
                   </div>
                   <div class="lightboximages">
                      <a href="{{asset('admin/color/'.$colorzoom->image)}}" data-size="1462x2048"></a>
@@ -257,8 +252,6 @@
          <ul class="product-tabs">
             <li rel="tab1"><a class="tablink">Product Details</a></li>
             <li rel="tab2"><a class="tablink">Product Reviews</a></li>
-            <li rel="tab3"><a class="tablink">Size Chart</a></li>
-            <li rel="tab4"><a class="tablink">Shipping &amp; Returns</a></li>
          </ul>
          <div class="tab-container">
             <div id="tab1" class="tab-content">
@@ -419,113 +412,7 @@
                   </div>
                </div>
             </div>
-            <div id="tab3" class="tab-content">
-               <h3>WOMEN'S BODY SIZING CHART</h3>
-               <table>
-                  <tbody>
-                     <tr>
-                        <th>Size</th>
-                        <th>XS</th>
-                        <th>S</th>
-                        <th>M</th>
-                        <th>L</th>
-                        <th>XL</th>
-                     </tr>
-                     <tr>
-                        <td>Chest</td>
-                        <td>31" - 33"</td>
-                        <td>33" - 35"</td>
-                        <td>35" - 37"</td>
-                        <td>37" - 39"</td>
-                        <td>39" - 42"</td>
-                     </tr>
-                     <tr>
-                        <td>Waist</td>
-                        <td>24" - 26"</td>
-                        <td>26" - 28"</td>
-                        <td>28" - 30"</td>
-                        <td>30" - 32"</td>
-                        <td>32" - 35"</td>
-                     </tr>
-                     <tr>
-                        <td>Hip</td>
-                        <td>34" - 36"</td>
-                        <td>36" - 38"</td>
-                        <td>38" - 40"</td>
-                        <td>40" - 42"</td>
-                        <td>42" - 44"</td>
-                     </tr>
-                     <tr>
-                        <td>Regular inseam</td>
-                        <td>30"</td>
-                        <td>30½"</td>
-                        <td>31"</td>
-                        <td>31½"</td>
-                        <td>32"</td>
-                     </tr>
-                     <tr>
-                        <td>Long (Tall) Inseam</td>
-                        <td>31½"</td>
-                        <td>32"</td>
-                        <td>32½"</td>
-                        <td>33"</td>
-                        <td>33½"</td>
-                     </tr>
-                  </tbody>
-               </table>
-               <h3>MEN'S BODY SIZING CHART</h3>
-               <table>
-                  <tbody>
-                     <tr>
-                        <th>Size</th>
-                        <th>XS</th>
-                        <th>S</th>
-                        <th>M</th>
-                        <th>L</th>
-                        <th>XL</th>
-                        <th>XXL</th>
-                     </tr>
-                     <tr>
-                        <td>Chest</td>
-                        <td>33" - 36"</td>
-                        <td>36" - 39"</td>
-                        <td>39" - 41"</td>
-                        <td>41" - 43"</td>
-                        <td>43" - 46"</td>
-                        <td>46" - 49"</td>
-                     </tr>
-                     <tr>
-                        <td>Waist</td>
-                        <td>27" - 30"</td>
-                        <td>30" - 33"</td>
-                        <td>33" - 35"</td>
-                        <td>36" - 38"</td>
-                        <td>38" - 42"</td>
-                        <td>42" - 45"</td>
-                     </tr>
-                     <tr>
-                        <td>Hip</td>
-                        <td>33" - 36"</td>
-                        <td>36" - 39"</td>
-                        <td>39" - 41"</td>
-                        <td>41" - 43"</td>
-                        <td>43" - 46"</td>
-                        <td>46" - 49"</td>
-                     </tr>
-                  </tbody>
-               </table>
-               <div class="text-center">
-                  <img src="{{asset('front/assets/images/size.jpg')}}" alt="" />
-               </div>
-            </div>
-            <div id="tab4" class="tab-content">
-               <h4>Returns Policy</h4>
-               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eros justo, accumsan non dui sit amet. Phasellus semper volutpat mi sed imperdiet. Ut odio lectus, vulputate non ex non, mattis sollicitudin purus. Mauris consequat justo a enim interdum, in consequat dolor accumsan. Nulla iaculis diam purus, ut vehicula leo efficitur at.</p>
-               <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. In blandit nunc enim, sit amet pharetra erat aliquet ac.</p>
-               <h4>Shipping</h4>
-               <p>Pellentesque ultrices ut sem sit amet lacinia. Sed nisi dui, ultrices ut turpis pulvinar. Sed fringilla ex eget lorem consectetur, consectetur blandit lacus varius. Duis vel scelerisque elit, et vestibulum metus.  Integer sit amet tincidunt tortor. Ut lacinia ullamcorper massa, a fermentum arcu vehicula ut. Ut efficitur faucibus dui Nullam tristique dolor eget turpis consequat varius. Quisque a interdum augue. Nam ut nibh mauris.</p>
-            </div>
-         </div>
+                     </div>
       </div>
       <!--End Product Tabs-->
       </div>
