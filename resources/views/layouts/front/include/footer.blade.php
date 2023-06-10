d5d0c4284e6d3eb808e91077e9f9c25185c9912c
avkashgoyal <edusemarketing0310@gmail.com>
2023-06-10 15:08:44 +0530

    chnges

--- resources/views/layouts/front/include/footer.blade.php
+++ resources/views/layouts/front/include/footer.blade.php
@@ -11,6 +11,12 @@
     <h5 class="headin5_amrc col_white_amrc pt2">Contact Us</h5>
     <p><i class="fa fa-phone"></i>  +91-9904145427</p>
     <p><i class="fa fa fa-envelope"></i> info@maiyee.in  </p>
+    <ul class="social_footer_ul">
+        <li><a href="https://www.facebook.com/profile.php?id=100070799291176&mibextid=ZbWKwL" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+        <li><a href="https://pin.it/2kbR7xN" target="_blank"><i class="fab fa-pinterest"></i></a></li>
+        <li><a href="https://youtube.com/@Maiyee_collection" target="_blank"><i class="fab fa-youtube"></i></a></li>
+        <li><a href="https://instagram.com/maiyee.in?igshid=ZDdkNTZiNTM=" target="_blank"><i class="fab fa-instagram"></i></a></li>
+        </ul>
     </div>
 
 
@@ -22,23 +28,10 @@
 <li><a href="{{route('web.refund')}}">Cancellation/Refund</a></li>
 <li><a href="{{route('web.shipping')}}">Delivery/Shipping</a></li>
 <li><a href="{{route('web.disclaimer')}}">Disclaimer</a></li>
-<li><a href="#">Hair Masking/Clipping</a></li>
-<li><a href="#">Image Cropping</a></li>
 </ul>
 <!--footer_ul_amrc ends here-->
 </div>
 
-
-<!--<div class=" col-sm-4 col-md  col-12 col">
-<h5 class="headin5_amrc col_white_amrc pt2">Follow us</h5>
-
-
-<ul class="footer_ul2_amrc">
-<li><a href="#"><i class="fab fa-instagram fleft padding-right"></i> </a><p>Lorem Ipsum is simply dummy text of the printing...<a href="#">https://www.lipsum.com/</a></p></li>
-<li><a href="#"><i class="fab fa-instagram fleft padding-right"></i> </a><p>Lorem Ipsum is simply dummy text of the printing...<a href="#">https://www.lipsum.com/</a></p></li>
-<li><a href="#"><i class="fab fa-instagram fleft padding-right"></i> </a><p>Lorem Ipsum is simply dummy text of the printing...<a href="#">https://www.lipsum.com/</a></p></li>
-</ul>
-</div>--->
 </div>
 </div>
 
