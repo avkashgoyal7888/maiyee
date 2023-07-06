2039288e58b780e8df415a78d317706eef84dabc
aneesh0987 <82146611+aneesh723@users.noreply.github.com>
2023-07-06 12:40:56 +0530

    'push'

--- resources/views/admin/email/order-invoice.blade.php
+++ resources/views/admin/email/order-invoice.blade.php
@@ -1,27 +1,21 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <style>
-        .invoice {
-            font-family: Arial, sans-serif;
-            margin: 0 auto;
-            max-width: 600px;
-            padding: 20px;
-            background-color: #f9f9f9;
-        }
-        
-        
-    </style>
-</head>
-<body>
-    <div class="invoice">
-        <h1>Order Invoice</h1>
-        <p>Order ID: {{ $orderCoupon['order_id'] }}</p>
-        <p>Customer Name: {{ $orderCoupon['name'] }}</p>
-        <p>Address: {{ $orderCoupon['address'] }}</p>
-        <!-- Add more invoice details here -->
+<p><span style="font-size:14px"><span style="font-family:Calibri,sans-serif"><span style="font-family:&quot;Segoe UI&quot;,sans-serif"><span style="color:black">Subject: Invoice for Your &ldquo;MAIYEE&rdquo; Purchase</span></span></span></span></p>
 
-        <p>Attached to this email is your invoice.</p>
-    </div>
-</body>
-</html>
+<p><span style="font-size:14px"><span style="font-family:Calibri,sans-serif"><span style="font-family:&quot;Segoe UI&quot;,sans-serif"><span style="color:black">Dear <strong>{{ $orderCoupon['name'] }}</strong>,</span></span></span></span></p>
+
+<p><span style="font-size:14px"><span style="font-family:Calibri,sans-serif"><span style="font-family:&quot;Segoe UI&quot;,sans-serif"><span style="color:black">We hope this email finds you well. Thank you for choosing us &ldquo;MAIYEE&rdquo; for your purchase. We are pleased to provide you with the details of your invoice. Please review the information below:</span></span></span></span></p>
+
+<p><span style="font-size:14px"><span style="font-family:Calibri,sans-serif"><span style="font-family:&quot;Segoe UI&quot;,sans-serif"><span style="color:black">Invoice Number: <strong>{{ $orderCoupon['invoice_num'] }} </strong></span></span></span></span></p>
+
+<p><span style="font-size:14px"><span style="font-family:Calibri,sans-serif"><span style="font-family:&quot;Segoe UI&quot;,sans-serif"><span style="color:black">Date: <strong>{{ $orderCoupon['order_date'] }} </strong></span></span></span></span></p>
+
+<p><span style="font-size:14px"><span style="font-family:Calibri,sans-serif"><span style="font-family:&quot;Segoe UI&quot;,sans-serif"><span style="color:black">Customer Name: <strong>{{ $orderCoupon['name'] }} </strong></span></span></span></span></p>
+
+<p><span style="font-size:14px"><span style="font-family:Calibri,sans-serif"><span style="font-family:&quot;Segoe UI&quot;,sans-serif"><span style="color:black">Shipping Address: {{ $orderCoupon['address'] }} {{ $orderCoupon['landmark'] }} {{ $orderCoupon['state'] }} {{ $orderCoupon['city'] }} {{ $orderCoupon['pin_code'] }}</span></span></span></span></p>
+
+<p><span style="font-size:14px"><span style="font-family:Calibri,sans-serif"><span style="font-family:&quot;Segoe UI&quot;,sans-serif"><span style="color:black">Payment Method: <strong>{{ $orderCoupon['payment_method'] }} </strong></span></span></span></span></p>
+
+<p><span style="font-size:14px"><span style="font-family:Calibri,sans-serif"><span style="font-family:&quot;Segoe UI&quot;,sans-serif"><span style="color:black">Transaction ID: <strong>{{ $orderCoupon['txnid'] }}</strong></span></span></span></span></p>
+
+<p><span style="font-size:14px"><span style="font-family:Calibri,sans-serif"><span style="font-family:&quot;Segoe UI&quot;,sans-serif"><span style="color:black">Please note that the total amount includes any applicable taxes and shipping charges. If you have any questions or concerns regarding this invoice, please don&#39;t hesitate to reach out to our customer support team at [info.maiyee_custumersupport.in] or [9904145427].</span></span></span></span></p>
+
+<p><span style="font-size:14px"><span style="font-family:Calibri,sans-serif"><span style="font-family:&quot;Segoe UI&quot;,sans-serif"><span style="color:black"><img alt="" src="https://res.cloudinary.com/dzujz2mkt/image/upload/v1688378123/maiyee.png" style="height:38px; width:100px" /></span></span></span></span></p>
