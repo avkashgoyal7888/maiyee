f75cd7cc2394568a10574f24a1976cde3ddb21f8
avkashgoyal <edusemarketing0310@gmail.com>
2023-08-07 11:41:04 +0530

    changes

--- resources/views/link/index.blade.php
+++ resources/views/link/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <!--Header-->
 <div class="header-wrap animated d-flex border-bottom">
-    <div class="container-fluid" style="width:100%">
+    <div class="container-fluid" style="width:100%; display:flex; justify-content:space-around">
         <div class="logo">
             <a href="#">
                 <img src="https://res.cloudinary.com/dzujz2mkt/image/upload/v1688378123/maiyee.png" height="55" width="110">
