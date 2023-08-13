a359ac386c6230fb743d5743689108d88beb0722
avkashgoyal <edusemarketing0310@gmail.com>
2023-08-14 02:06:47 +0530

    changes

--- resources/views/link/index.blade.php
+++ resources/views/link/index.blade.php
@@ -21,7 +21,7 @@
 @section('content')
 <!--Header-->
 <div class="header-wrap animated d-flex border-bottom">
-    <div class="container-fluid mb-10" style="position: fixed; z-index:10; background-color:#fff;">
+    <div class="container-fluid" style="position: fixed; z-index:10; background-color:#fff;">
         <div class="row align-items-center">
             <!--Desktop Logo-->
             <div class="logo col-md-2 col-lg-2 d-none d-lg-block">
@@ -50,7 +50,7 @@
     </div>
 </div>
 <!--End Header-->
-<div class="slideshow slideshow-wrapper pb-section">
+<div class="slideshow slideshow-wrapper pb-section" style="margin-top: 100px">
    <div class="home-slideshow">
       @foreach($banner as $banners)
       <div class="slide">
