07e97a4a4580a08e892210176706fab99c4f6a24
avkashgoyal <edusemarketing0310@gmail.com>
2023-08-14 01:59:18 +0530

    changes

--- resources/views/link/index.blade.php
+++ resources/views/link/index.blade.php
@@ -20,7 +20,7 @@
 @stop
 @section('content')
 <!--Header-->
-<div class="header-wrap animated d-flex border-bottom">
+<div class="header-wrap animated d-flex border-bottom" style="position: fixed">
     <div class="container-fluid" style="width:100%">
         <div class="row align-items-center">
             <!--Desktop Logo-->
