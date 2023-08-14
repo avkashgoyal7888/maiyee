67d3d0b2723c9782bea812cc3415ff2252a087c2
aneesh0987 <82146611+aneesh723@users.noreply.github.com>
2023-08-14 12:23:13 +0530

    'push'

--- app/Http/Livewire/Admin/Linkcategory/Order.php
+++ app/Http/Livewire/Admin/Linkcategory/Order.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Builder;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Exports\LinkUserOrderExport;
+use PDF;
 
 class Order extends Component
 {
@@ -42,6 +43,41 @@ class Order extends Component
 
         return Excel::download(new LinkUserOrderExport($data,$user), 'accountstatement.xlsx');
     }
+
+    public function exportPDF()
+    {
+        $data = LinkUserProduct::with('product.cat')
+            ->where('user_id', $this->user_id)
+            ->whereHas('product', function (Builder $query) {
+                if ($this->search) {
+                    $searchTerm = '%' . $this->search . '%';
+                    $query->where('product_name', 'like', $searchTerm)
+                        ->orWhere('style_code', 'like', $searchTerm);
+                }
+            })
+            ->orderByDesc('id')
+            ->get();
+
+        $user = LinkUser::where('id', $this->user_id)->first();
+
+        $pdf = PDF::loadView('link.pdf', ['data' => $data, 'user' => $user]);
+        $pdf->setPaper('A4', 'portrait');
+        $pdfContent = $pdf->output();
+
+        $headers = [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'inline; filename="' . $user->name . '.pdf"',
+        ];
+
+        return response()->stream(
+            function () use ($pdfContent) {
+                echo $pdfContent;
+            },
+            200,
+            $headers
+        );
+    }
+
 
     public function render()
     {
--- resources/views/link/pdf.blade.php
+++ resources/views/link/pdf.blade.php
@@ -1,36 +1,50 @@
-<div>
-   <div class="card-body">
-    <div class="form-group col-md-3 col-lg-3 col-sm-12 col-12 mb-3">
-         <label for="search">Search</label>
-         <input type="text" wire:model="search" class="form-control" id="search">
-      </div>
-      <div class="table-responsive" style="margin-top: 20px;">
-         <table class="table table-sm table-bordered border-dark mb-0 text-center">
-            <button type="button" class="btn btn-sm btn-success" wire:click="exportExcel"><i class='bx bxs-file-export'></i>Excel</button>
-            <button type="button" class="btn btn-sm btn-success" wire:click="exportPDF"><i class='bx bxs-file-export'></i>PDF</button>
-            <tbody>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    
+    <style>
+        /* Add your table border style here */
+        table {
+            border-collapse: collapse;
+            width: 100%;
+            border: 1px solid black; /* Border around the whole table */
+        }
+        th, td {
+            border: 1px solid black; /* Border for table cells */
+            padding: 8px;
+            text-align: left;
+        }
+    </style>
+  </head>
+
+  <body>
+  	<div class="table-responsive" style="margin-top: 20px;">
+         <table>
+            <tbody style="WI">
                <tr>
                   <td colspan="3">Name</td>
-                  <td colspan="3">{{$user->name}}</td>
+                  <td colspan="4">{{$user->name}}</td>
                </tr>
                <tr>
                   <td colspan="3">Number</td>
-                  <td colspan="3">{{$user->number}}</td>
+                  <td colspan="4">{{$user->number}}</td>
                </tr>
                <tr>
                   <td colspan="3">Address</td>
-                  <td colspan="3">{{$user->address}}</td>
+                  <td colspan="4">{{$user->address}}</td>
                </tr>
                <tr>
                   <td colspan="3">Size</td>
-                  <td colspan="3">{{$user->size}}</td>
+                  <td colspan="4">{{$user->size}}</td>
                </tr>
                <tr>
                   <td colspan="3">Slot</td>
-                  <td colspan="3">Date : {{$user->delivery_date}} Time: {{$user->start_time}}-{{$user->end_time}}</td>
+                  <td colspan="4">Date : {{$user->delivery_date}} Time: {{$user->start_time}}-{{$user->end_time}}</td>
                </tr>
                <tr>
-                  <td colspan="6"></td>
+                  <td colspan="7"></td>
                </tr>
             </tbody>
          </tbody>
@@ -41,6 +55,7 @@
                   <td>Style Code</td>
                   <td>MRP</td>
                   <td>Selling Price</td>
+                  <th>Remarks</th>
                </tr>
             </tbody>
             <tbody>
@@ -52,6 +67,7 @@
                   <td>{{ ucwords($order->product->style_code) }}</td>
                   <td>{{ ucwords($order->product->mrp) }}</td>
                   <td>{{ ucwords($order->product->selling_price) }}</td>
+                  <td>  </td>
                </tr>
                @empty
                <tr>
@@ -61,5 +77,7 @@
             </tbody>
          </table>
       </div>
-   </div>
-</div>
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+</body>
+
